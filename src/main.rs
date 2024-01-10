a2ed13e42252451a712acc3c3d5be73948d68670
fizlip <fillatino@gmail.com>
2024-01-10 10:11:24 +0100

    add tests and docs

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,17 @@ use scraper::html::Select;
 use std::iter::Enumerate;
 use scraper::ElementRef;
 use regex::Regex;
+use std::fs;
+use more_asserts::assert_le;
 
 const PAGE_SIZE:usize = 20;
 const LINK_TAG: &str = "a.sc-3189427c-0";
 const RAW_TEXT_TAG: &str = "div.body-text";
 
+/// main_page_links will retrieve all law links from www.riksdagen.se
+/// @params i -- the page to retrieve
+/// @params take -- the page size
+/// @returns Vec<String> -- vector of links to all laws on the page
 fn main_page_links(i: u64, take: usize) -> Vec<String>{
     // Get HTML 
     let url = format!("https://www.riksdagen.se/sv/sok/?doktyp=sfs&dokstat=g%C3%A4llande+sfs&p={}",i);
@@ -17,6 +23,16 @@ fn main_page_links(i: u64, take: usize) -> Vec<String>{
     iter
 }
 
+/// extract_tag will return an array will all html tags from a url that match the 
+/// tag given.
+/// ignore and take define the section of the page to select attr defines the 
+/// attribute we want to extract from each tag.
+/// @param url -- site to visit 
+/// @param ignore -- the amount of tags from the first index to ignore, similar to n..
+/// @param take -- the amount of tags to select, usually the page size in a paginated list
+/// @param tag -- the html tag to select e.g. a.price-link, div.body-text
+/// @param attr -- html attribute to select e.g. href
+/// @return Vec<String> -- a vector of tags of length take that match the tag and attribute
 fn extract_tag(url: &str, ignore: usize, take: usize, tag: &str, attr: &str) -> Vec<String>{
     // Get HTML 
     let mut _ignore = 0;
@@ -46,12 +62,17 @@ fn extract_tag(url: &str, ignore: usize, take: usize, tag: &str, attr: &str) -> 
     res
 }
 
-fn write_paragraphs(url: &str) {
+/// write_paragraphs will be given a url with a law and return a vector of
+/// all paragraphs on the site
+/// @param url: &str -- the url to visit
+/// @returns Vec<String> -- paragraphs
+fn write_paragraphs(url: &str) -> Vec<String> {
+
     let re: regex::Regex = Regex::new(r"^((\d|\s)*§)").unwrap();
+    let mut paragraphs: Vec<String> = vec![];
 
     println!("{:?}", url);
     let raw_text_1 = extract_tag(url, 0, 1, RAW_TEXT_TAG, "")[0].replace("\t", "");
-
     let raw_text_1 = raw_text_1.split("\n");
 
     let mut current:Vec<&str> = vec![]; 
@@ -64,19 +85,26 @@ fn write_paragraphs(url: &str) {
             current.push(line);
         }
         else{
-            println!("{:?}\n", current.join(" "));
+            paragraphs.push(current.join(" "));
             current.clear();
             current.push(line);
         }
     }
-    println!("{:?}\n", current.join(" "));
+    paragraphs.push(current.join(" "));
+
+    paragraphs
 
 }
 
-fn write_full_text(url: &str) {
+/// write_full_text will extract the raw text content from a url using the 
+/// RAW_TEXT_TAG
+/// @params url: &str -- the url to get content from
+/// @returns String -- string with text content from the website, no newline or tabs
+fn write_full_text(url: &str) -> String{
     let raw_text = extract_tag(url, 0, 1, RAW_TEXT_TAG, "")[0].replace("\t", "");
     let raw_text = raw_text.replace("\n", "").replace("\t", "");
-    println!("{:?}\n", raw_text);
+
+    raw_text
 }
 
 fn main() {
@@ -88,7 +116,10 @@ fn main() {
 
     for link in links {
         let url = &extract_tag(&link, 3, 1, LINK_TAG, "href")[0];
-        write_full_text(url)
+        //let text = write_full_text(url);
+        let ps = write_paragraphs(url);
+
+        println!("{:?}", ps);
     }
 
 }
@@ -99,20 +130,20 @@ mod tests {
 
     #[test]
     fn first_link_is_correct() {
-        let p1 = main_page_links(1);
+        let p1 = main_page_links(1, PAGE_SIZE);
 
         assert_eq!(p1[0], "https://www.riksdagen.se/sv/dokument-och-lagar/dokument/svensk-forfattningssamling/tillkannagivande-2023954-av-uppgift-om_sfs-2023-954/");
         assert_eq!(p1[19], "https://www.riksdagen.se/sv/dokument-och-lagar/dokument/svensk-forfattningssamling/lag-2023714-om-forvarv-och-forvaltning-av_sfs-2023-714/");
     }
     #[test]
     fn is_wrong() {
-        let p1 = main_page_links(1);
+        let p1 = main_page_links(1, PAGE_SIZE);
 
         assert_ne!(p1[0], "not a link");
     }
     #[test]
     fn first_link_last_page_is_correct() {
-        let p1 = main_page_links(268);
+        let p1 = main_page_links(268, PAGE_SIZE);
 
         assert_eq!(p1[0], "https://www.riksdagen.se/sv/dokument-och-lagar/dokument/svensk-forfattningssamling/for-riksbankens-styrelse-och-forvaltning_c5s0riksb/");
         assert_eq!(p1[19], "https://www.riksdagen.se/sv/dokument-och-lagar/dokument/svensk-forfattningssamling/byggningabalk-17360123-1_sfs-1736-0123%201/");
@@ -120,10 +151,34 @@ mod tests {
 
     #[test]
     fn link_random_page_is_correct() {
-        let p1 = main_page_links(123);
+        let p1 = main_page_links(123, PAGE_SIZE);
 
         assert_eq!(p1[0], "https://www.riksdagen.se/sv/dokument-och-lagar/dokument/svensk-forfattningssamling/forordning-2006421-om-sakerhet-i-vagtunnlar_sfs-2006-421/");
         assert_eq!(p1[19], "https://www.riksdagen.se/sv/dokument-och-lagar/dokument/svensk-forfattningssamling/lag-2006228-med-sarskilda-bestammelser-om_sfs-2006-228/");
         assert_eq!(p1[10], "https://www.riksdagen.se/sv/dokument-och-lagar/dokument/svensk-forfattningssamling/lag-2006323-om-utbyte-av-sprutor-och-kanyler_sfs-2006-323/");
+    }
+
+    #[test]
+    fn raw_text_is_correct_random_law() {
+        let url = "http://rkrattsbaser.gov.se/sfst?bet=2023:903"; 
+        let text = write_full_text(url);
+        let correct = fs::read_to_string("/home/filip/Dokument/lawgpt/thescraper/src/test/raw_text_test_1.txt")
+            .expect("Can't read ./test/raw_text_test_1.txt")
+            .replace("\t", "")
+            .replace("\n", "");
+
+        assert_le!(text.len(), correct.len());
+        assert!(text.contains("Träder i kraft I:2024-04-02"));
+        assert!(text.contains("i mån av tillgång på medel, ges till en kommun som efter den 31 december 2023 antar e"));
+        assert!(text.contains("Överklagande 15 § Beslut enligt denna förordning får inte överklagas."));
+    }
+
+    #[test]
+    fn paragraphs_is_correct_random_law() {
+        let url = "http://rkrattsbaser.gov.se/sfst?bet=2023:903"; 
+        let ps = write_paragraphs(url);
+        assert!(ps[1].contains("1 § I denna förordning finns bestämmelser om stöd"));
+        assert!(ps[1].contains("- 8 kap. 7 § regeringsformen i fråga om övriga bestämmelser."));
+        assert!(ps[15].contains("Beslut enligt denna förordning får inte överklagas."));
     }
 }
